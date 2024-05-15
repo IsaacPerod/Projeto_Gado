886bdcc40966eece60da72181bdf15d5b6ca0b72
Isaac Pedroso Rodrigues <isaacrodrigues.aluno@unipampa.edu.br>
2024-05-15 18:47:41 -0300

    primeiro commit

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final controller = TextEditingController();
+
+  final list = <String>[];
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pesagem de Gado',
+        style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/cow.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const FlutterLogo(
+                    size: 200,
+                    textColor: Colors.white,
+                    style:FlutterLogoStyle.horizontal,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Iniciar Pesagem'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                  ),
+                  )
+                ]
+              )
+            )
+          ]
+        )
+        
+      )
+    );
+  }
+}
