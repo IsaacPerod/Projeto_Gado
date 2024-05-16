a4191849c8847edfcf807ee195f2e865f18815ef
Isaac Pedroso Rodrigues <isaacrodrigues.aluno@unipampa.edu.br>
2024-05-16 02:50:36 -0300

    assets

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -117,7 +117,7 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      SizedBox(width: 150),
+                      const SizedBox(width: 150),
                       InkWell(
                         onTap: () {
                           print('Esqueci minha Senha foi clicado');
